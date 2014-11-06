9bbc8bafdb49b64b16e264772b9d954bddd4fa09
Patrick Lynch <pelynch@wpi.edu>
2014-11-06 01:51:08 -0500

    This is bad and I feel bad. pelynch

--- NumberGame/Form1.Designer.cs
+++ NumberGame/Form1.Designer.cs
@@ -45,6 +45,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -54,6 +55,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -63,6 +65,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -72,11 +75,12 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Location = new System.Drawing.Point(93, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 4;
@@ -85,7 +89,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 8);
+            this.label2.Location = new System.Drawing.Point(93, 46);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 5;
@@ -94,7 +98,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 125);
+            this.label3.Location = new System.Drawing.Point(43, 125);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 6;
@@ -104,7 +108,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 151);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -114,6 +118,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- NumberGame/Form1.cs
+++ NumberGame/Form1.cs
@@ -12,9 +12,101 @@ namespace NumberGame
 {
     public partial class Form1 : Form
     {
+        int[] val = { 0, 0, 2, 2, 3 };
+        int c = -1,count = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = val[1].ToString();
+            button2.Text = val[2].ToString();
+            button3.Text = val[3].ToString();
+            button4.Text = val[4].ToString();
+            label1.Text = val[0].ToString();
+            label2.Text = "";
+            label3.Text = "+";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            update(1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            update(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            update(3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            update(4);
+        }
+
+        private void update(int button)
+        {
+            int v = -1 * (((count % 2) * 2) - 1);
+            if (1 == v)
+            {
+                label3.Text = "+";
+            }
+            else if (-1 == v)
+            {
+                label3.Text = "-";
+            }
+            v *= val[button];
+            count++;
+            label1.Text = count.ToString();
+            for (int i = 1; i <= 4; i++)
+            {
+                if (i == button)
+                {
+                    continue;
+                }
+                val[i] += v;
+            }
+            label2.Text += button.ToString();
+            switch (button)
+            {
+                case 1:
+                    button2.Text = val[2].ToString();
+                    button3.Text = val[3].ToString();
+                    button4.Text = val[4].ToString();
+                    break;
+                case 2:
+                    button1.Text = val[1].ToString();
+                    button3.Text = val[3].ToString();
+                    button4.Text = val[4].ToString();
+                    break;
+                case 3:
+                    button1.Text = val[1].ToString();
+                    button2.Text = val[2].ToString();
+                    button4.Text = val[4].ToString();
+                    break;
+                case 4:
+                    button1.Text = val[1].ToString();
+                    button2.Text = val[2].ToString();
+                    button3.Text = val[3].ToString();
+                    break;
+                default:
+                    break;
+            }
+            if (0 == (label2.Text.Length - c) % 20 && label2.Text.Length > 1)
+            {
+                label2.Text += "\n";
+            }
+            if (val[1] == val[2] && val[2] == val[3] && val[3] == val[4])
+            {
+                label1.Text = "You won in " + label1.Text + " moves!";
+            }
         }
     }
 }
